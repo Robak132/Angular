ab5bf3b1ed73eb890302cd746827dde047058796
Jakub Robaczewski <kubarob13@gmail.com>
2022-06-06 16:21:31 +0200

    Added new navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { CounterComponent } from './counter/counter.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { MessagesComponent } from './messages/messages.component';
     HeroesComponent,
     CounterComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
